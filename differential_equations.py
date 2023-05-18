f3f285ce0f5295fecb715e68287eb7c42b9b02d9
Andryss <andr.krivosheev@gmail.com>
2023-05-18 19:36:16 +0300

    Add example differential equation

--- differential_equations.py
+++ differential_equations.py
@@ -15,8 +15,24 @@ class OrdinaryDifferentialEquation:
     def answer_at(self, x: float):
         return self.actual_solution.at(x)
 
+    def __str__(self):
+        return f"y' = {self.func.string()}"
+
+
+def get_example_1_differential_equation() -> OrdinaryDifferentialEquation:
+    return OrdinaryDifferentialEquation(
+        TwoVariableFunction(
+            "y + (1 + x) * y^2",
+            lambda x, y: y + (1 + x) * y**2
+        ),
+        Function(
+            "-1/x",
+            lambda x: -1/x
+        )
+    )
+
 
 def get_all_differential_equations() -> list[OrdinaryDifferentialEquation]:
     return [
-
+        get_example_1_differential_equation()
     ]
