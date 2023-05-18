66fcb4f02da5720dda36cc62c0514ec9d96d77b4
Andryss <andr.krivosheev@gmail.com>
2023-05-19 00:19:07 +0300

    Add main pipeline

--- main.py
+++ main.py
@@ -1,16 +1,138 @@
-# This is a sample Python script.
+import sys
+import warnings
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+from solvers import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def read_int_from_console(number_name: str) -> int:
+    print(f"\nEnter {number_name}:")
+    line = input().strip()
+    try:
+        return int(line)
+    except Exception as e:
+        raise Exception(f"Can't read int value: {e.__str__()}")
 
 
-# Press the green button in the gutter to run the script.
+def read_float_from_console(number_name: str) -> float:
+    print(f"\nEnter {number_name}:")
+    line = input().strip()
+    try:
+        return float(line)
+    except Exception as e:
+        raise Exception(f"Can't read float value: {e.__str__()}")
+
+
+def read_differential_equation() -> OrdinaryDifferentialEquation:
+    print("\nChoose the differential equation:")
+    differential_equations = get_all_differential_equations()
+    for i, equation in enumerate(differential_equations):
+        print(f"{i}\t{equation}")
+    try:
+        line = input("(enter the number) ").strip()
+        index = int(line)
+        return differential_equations[index]
+    except Exception as e:
+        raise Exception(f"can't read differential equation: {e.__str__()}")
+
+
+def read_start_point() -> [float, float]:
+    line = input("\nEnter the start point (like \"<x> <y>\"):\n").strip()
+    interval: [float, float]
+    try:
+        interval = [float(x) for x in line.split()]
+        if len(interval) != 2:
+            raise Exception("not a start point")
+        return interval
+    except Exception as e:
+        raise Exception(f"can't read start point: {e.__str__()}")
+
+
+def read_interval() -> [float, float]:
+    line = input("\nEnter the interval boundaries:\n").strip()
+    interval: [float, float]
+    try:
+        interval = [float(x) for x in line.split()]
+        if len(interval) != 2 or interval[1] < interval[0]:
+            raise Exception("not an interval")
+        return interval
+    except Exception as e:
+        raise Exception(f"can't read interval: {e.__str__()}")
+
+
+def read_step_size() -> float:
+    return read_float_from_console("step size")
+
+
+def read_precision() -> float:
+    return read_float_from_console("precision")
+
+
+def print_result_entity(micro_result: SolveResultEntity):
+    if isinstance(micro_result, SolveResultEntitySuccess):
+        print(f"\n{micro_result.name}")
+        print(f"{micro_result.approximated_values_table.table().T}")
+    elif isinstance(micro_result, SolveResultEntityError):
+        print(f"\n{micro_result.name}")
+        print(f"{micro_result.error.__str__()}")
+
+
+def read_bool(text: str) -> bool:
+    line = input(f"{text} [Y/y for YES, NO otherwise]: ").strip().lower()
+    if line == 'y':
+        return True
+    else:
+        return False
+
+
+def read_show_all() -> bool:
+    return read_bool("\nDo you want to see all possible plots?")
+
+
+def show_result_entity(micro_result: SolveResultEntity, info: DifferentialEquationInfo):
+    warnings.filterwarnings("ignore", category=matplotlib.MatplotlibDeprecationWarning)
+
+    if isinstance(micro_result, SolveResultEntitySuccess):
+        fig, ax = plt.subplots()
+
+        table = micro_result.approximated_values_table
+        ax.scatter(table.x_values(), table.y_values(), c='blue', label=micro_result.name)
+
+        x_vals = np.linspace(info.interval[0], info.interval[1], 20)
+        y_vals = np.vectorize(lambda x: info.equation.answer_at(x, info.start_point))(x_vals)
+
+        ax.scatter(x_vals, y_vals, c='red', label='real function')
+
+        ax.set_title(micro_result.name)
+        ax.legend()
+        fig.show()
+
+
+def print_result(result: SolveResult, info: DifferentialEquationInfo):
+    print("\nHere is the computation result:")
+    for micro_result in result.solving_results:
+        print_result_entity(micro_result)
+    if read_show_all():
+        for micro_result in result.solving_results:
+            show_result_entity(micro_result, info)
+
+
+def run():
+    try:
+        equation_info = DifferentialEquationInfo()
+        equation_info.equation = read_differential_equation()
+        equation_info.start_point = read_start_point()
+        equation_info.interval = read_interval()
+        equation_info.step_size = read_step_size()
+        equation_info.precision = read_precision()
+        result = solve_equation(equation_info)
+        print_result(result, equation_info)
+    except Exception as e:
+        print(f'Some error occurred: {e}', file=sys.stderr)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    run()
