fbc9d0008010cc34887dadcc940988b5f55a134f
Andryss <andr.krivosheev@gmail.com>
2023-05-18 15:47:29 +0300

    Update differential equations

--- differential_equations.py
+++ differential_equations.py
@@ -2,12 +2,21 @@ from functions import *
 
 
 class OrdinaryDifferentialEquation:
-    string: str
     func: TwoVariableFunction
+    actual_solution: Function
 
-    def __init__(self, s, f):
-        self.string = s
+    def __init__(self, f, s):
         self.func = f
+        self.actual_solution = s
 
     def at(self, x: float, y: float):
         return self.func.at(x, y)
+
+    def answer_at(self, x: float):
+        return self.actual_solution.at(x)
+
+
+def get_all_differential_equations() -> list[OrdinaryDifferentialEquation]:
+    return [
+
+    ]
